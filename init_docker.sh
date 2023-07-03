645448a8f02c665489c2ff60c094fb2b27041a2d
Casper-Min <ycivil93@hanyang.ac.kr>
2023-07-03 19:05:28 +0900

    modified init_docker.sh

--- init_docker.sh
+++ init_docker.sh
@@ -2,4 +2,4 @@ cur_dir=$(pwd)
 DIR=$cur_dir/host_dir
 mkdir -p ${DIR}
 docker build -t nsight:cuda12.1.1-ubt22.04-nsight12.1 .
-docker run -it --privileged --gpus all --name sd_profile -v ${DIR}:/root nsight:cuda12.1.1-ubt22.04-nsight12.1 /bin/bash
+docker run -it --privileged --gpus all --name sd_profile -v ${DIR}:/root/docker_dir nsight:cuda12.1.1-ubt22.04-nsight12.1 /bin/bash
