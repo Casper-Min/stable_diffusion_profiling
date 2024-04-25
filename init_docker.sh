759ef7ede01f7060b2262b0e7b767c62ae80b8d0
Minyong Yoon <ycivil93@hanyang.ac.kr>
2024-04-25 18:36:07 +0900

    Update init_docker.sh

--- init_docker.sh
+++ init_docker.sh
@@ -1,5 +1,5 @@
 cur_dir=$(pwd)
 DIR=$cur_dir/host_dir
 mkdir -p ${DIR}
-docker build -t nsight:cuda12.1.1-ubt22.04-nsight12.1 .
-docker run -it --privileged --gpus all --name sd_profile -v ${DIR}:/root/docker_dir nsight:cuda12.1.1-ubt22.04-nsight12.1 /bin/bash
+docker build -t nsight:cuda12.4.1-ubt22.04-nsight12.4 .
+docker run -it --privileged --gpus all --name sd_profile -v ${DIR}:/root/docker_dir nsight:cuda12.4.1-ubt22.04-nsight12.4 /bin/bash
